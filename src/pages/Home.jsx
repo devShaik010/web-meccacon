c1f481ce66ca18fd90c0c89860fd5adfbce888ee
atybdot <180611249+atybdot@users.noreply.github.com>
2025-02-28 02:19:24 +0530

    fix:change div to React.Fragment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,10 @@ import Hero from "@components/Hero/Hero";
 import Carousel from "@components/Carousel/Carousel";
 const Home = () => {
   return (
-    <div>
+    <>
       <Hero />
-      
       <Carousel />
-    </div>
+    </>
   );
 };
 
