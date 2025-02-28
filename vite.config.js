71021d0c6cd2241a131f441633602dd134779a5a
atybdot <180611249+atybdot@users.noreply.github.com>
2025-03-01 02:56:12 +0530

    feat:move to jsconfig
    instead of resolving paths directly in vite-config.
    utilize jsconfig.
    This will give us 2 main benefits:
    1. better intellisense
    2. vite-jsconfig-paths will resolve paths for us

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+
 import tailwindcss from "@tailwindcss/vite";
+import jsconfig from "vite-jsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@src": path.resolve(__dirname, "src"),
-      "@assets": path.resolve(__dirname, "src/assets"),
-      "@components": path.resolve(__dirname, "src/components"),
-      "@pages": path.resolve(__dirname, "src/pages"),
-      "@styles": path.resolve(__dirname, "src/styles"),
-      "@utils": path.resolve(__dirname, "src/utils"),
-    },
-  },
+  plugins: [react(), tailwindcss(), jsconfig()],
 });
