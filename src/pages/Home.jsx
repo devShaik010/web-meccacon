4247aaba243be1b536b9257e38d781aafda1988e
atybdot <180611249+atybdot@users.noreply.github.com>
2025-03-01 02:45:41 +0530

    chore:add bento grid in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Hero from "@components/Hero/Hero";
 import Carousel from "@components/Carousel/Carousel";
+import Bento from "@components/ui/Bento";
 const Home = () => {
   return (
     <>
       <Hero />
       <Carousel />
+      <Bento />
     </>
   );
 };
