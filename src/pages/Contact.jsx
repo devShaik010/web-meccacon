44a82e28672392ba2afd7b824005cd5552cc7844
atybdot <180611249+atybdot@users.noreply.github.com>
2025-03-03 01:03:40 +0530

    wip ;not completed

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,23 +1,62 @@
 import Form from "@components/ui/ContactForm";
-import img from "@assets/images/pexels-500photos-com-15338-93400.jpg";
+import img from "@assets/fc.svg";
 import { cn } from "@utils/cn";
+import { Accordion } from "../components/ui/Accordian";
 const Contact = () => {
+  const accordionItems = [
+    {
+      title: "Explore Knowledge",
+      content:
+        "Discover new worlds of information with our interactive learning platform.",
+    },
+    {
+      title: "Innovative Learning",
+      content:
+        "Experience cutting-edge educational techniques that adapt to your unique learning style.",
+    },
+    {
+      title: "Global Community",
+      content:
+        "Connect with learners worldwide and share insights in our vibrant, diverse community.",
+    },
+    {
+      title: "Personalized Growth",
+      content:
+        "Track your progress and receive tailored recommendations to accelerate your learning journey.",
+    },
+  ];
   return (
-    <div className=" bg-white px-4 py-8 lg:px-12 grid sm:grid-cols-2 md:py-12 place-items-center border-red-500">
-      <Form />
-      <div className=" w-full h-full hidden sm:flex rounded-xl relative">
-        <div
+    <section>
+      <div className=" bg-white px-4 py-8 lg:px-12 flex items-center flex-col justify-center md:grid md:grid-cols-2 md:py-12 place-items-center">
+        {/* <Form /> */}
+        <div className=" w-2/3  flex rounded-xl relative">
+          {/* <div
           className={cn(
             "top-0 left-0 w-full h-full absolute rounded-xl opacity-100",
             "bg-radial-[at_25%_25%] from-transparent to-primary/50 to-75%"
           )}
-        />
-        <img
-          src={img}
-          className="h-full max-h-[75svh] w-full object-cover rounded-lg "
-        />
+        /> */}
+          <img src={img} className="aspect-square w-full rounded-lg " />
+        </div>
+        <Accordion items={accordionItems} />
       </div>
-    </div>
+      <div className=" bg-white px-4 py-8 lg:px-12 flex items-center flex-col justify-center md:grid md:grid-cols-2 md:py-12 place-items-center">
+        <Form />
+        <div className=" w-2/3 flex rounded-xl relative">
+          <div
+            className={cn(
+              "top-0 left-0 w-full h-full absolute rounded-xl opacity-100",
+              "bg-radial-[at_25%_25%] from-transparent to-primary/50 to-75%"
+            )}
+          />
+          <img
+            src={img}
+            className="aspect-square hidden md:block w-full rounded-lg "
+          />
+        </div>
+        {/* <Accordion items={accordionItems} /> */}
+      </div>
+    </section>
   );
 };
 
